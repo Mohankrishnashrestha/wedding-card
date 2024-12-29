e042b30f9ac322f123d70b6847ffe37652610a70
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2024-12-29 21:35:23 +0545

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Content from "./content";
+import "./App.css";
 
 function App() {
+  const heading = {
+    topic: "Wedding Reception",
+    invite:
+      "We invite your plesure to come to the wedding recption ceremony of",
+    name1: "ADELINE",
+    and: "&",
+    name2: "ALEXGENDER",
+    day: "Saturday",
+    date: "26 January",
+    time: "At 10 am",
+    palace: "Smart Venue",
+  };
   return (
-    <div>weeding card</div>
-  )
+    <div>
+      <Content heading={heading} />
+    </div>
+  );
 }
 
-export default App
+export default App;
